fdcbcfdc57d3f1a0e9cc44cd0e380ac7f9feb008
Feng Hu <fenghu428@gmail.com>
2025-03-02 00:31:36 -0800

    update backend

--- api/index.js
+++ api/index.js
@@ -4,11 +4,11 @@ import cors from "cors";
 import morgan from "morgan";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "./models/User.js"; 
-import Place from "./models/Place.js"; 
+import User from "./models/User.js";
+import Place from "./models/Place.js";
 import cookieParser from "cookie-parser";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -18,7 +18,8 @@ const bcryptSaltRounds = 10;
 const jwtSecret = process.env.JWT_SECRET;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
@@ -32,8 +33,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 const corsOptions = {
-  origin: 'https://full-stack-explore-vancouver.vercel.app',
-  credentials: true, 
+  origin: [
+    "https://full-stack-explore-vancouver.vercel.app",
+    "http://localhost:3000", // Add localhost for development
+  ],
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
@@ -41,71 +45,37 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-app.get('/api/places', async (req, res) => {
+app.get("/api/places", async (req, res) => {
   const { category, search, ids } = req.query;
 
   try {
-      let query = {};
-      if (category) {
-          query.category = category;
-      }
-      if (search) {
-          query.title = { $regex: search, $options: 'i' };
-      }
-      if (ids) {
-          const objectIdArray = ids.map(id => new mongoose.Types.ObjectId(id));
-          query._id = { $in: objectIdArray };
-      }
-
-      const places = await Place.find(query);
-      res.json(places);
-  } catch (error) {
-      console.error("Error in /api/places:", error);
-      res.status(500).json({ message: error.message });
-  }
-});
-
-app.get('/api/user/:userId/favorites', async (req, res) => {
-  const { userId } = req.params;
-
-  try {
-      const user = await User.findById(userId).populate('favorites');
-      if (!user) {
-          return res.status(404).json('User not found');
-      }
-
-      res.json(user.favorites);
-  } catch (e) {
-      console.error("Error fetching favorites:", e);
-      res.status(500).json({ message: "Error fetching favorites", error: e });
-  }
-});
-
-
-app.get('/api/places/:id', async (req, res) => {
-  try {
-    const place = await Place.findById(req.params.id);
-    if (!place) {
-      return res.status(404).json('Place not found');
+    let query = {};
+    if (category) {
+      query.category = category;
     }
-    res.json(place);
+    if (search) {
+      query.title = { $regex: search, $options: "i" };
+    }
+    if (ids) {
+      const objectIdArray = ids.map((id) => new mongoose.Types.ObjectId(id));
+      query._id = { $in: objectIdArray };
+    }
+
+    const places = await Place.find(query);
+    res.json(places);
   } catch (error) {
+    console.error("Error in /api/places:", error);
     res.status(500).json({ message: error.message });
   }
 });
 
-
-app.get('/api/user/:userId/favorites/:category', async (req, res) => {
-  const { userId, category } = req.params;
+app.get("/api/user/:userId/favorites", async (req, res) => {
+  const { userId } = req.params;
 
   try {
-    const user = await User.findById(userId).populate({
-      path: 'favorites',
-      match: { category: category }
-    });
-
+    const user = await User.findById(userId).populate("favorites");
     if (!user) {
-      return res.status(404).json('User not found');
+      return res.status(404).json("User not found");
     }
 
     res.json(user.favorites);
@@ -115,14 +85,46 @@ app.get('/api/user/:userId/favorites/:category', async (req, res) => {
   }
 });
 
-app.post('/api/user/:userId/favorites', async (req, res) => {
+app.get("/api/places/:id", async (req, res) => {
+  try {
+    const place = await Place.findById(req.params.id);
+    if (!place) {
+      return res.status(404).json("Place not found");
+    }
+    res.json(place);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/user/:userId/favorites/:category", async (req, res) => {
+  const { userId, category } = req.params;
+
+  try {
+    const user = await User.findById(userId).populate({
+      path: "favorites",
+      match: { category: category },
+    });
+
+    if (!user) {
+      return res.status(404).json("User not found");
+    }
+
+    res.json(user.favorites);
+  } catch (e) {
+    console.error("Error fetching favorites:", e);
+    res.status(500).json({ message: "Error fetching favorites", error: e });
+  }
+});
+
+app.post("/api/user/:userId/favorites", async (req, res) => {
   const { userId } = req.params;
   const { itemId } = req.body;
 
   try {
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     if (!user.favorites.includes(itemId)) {
@@ -130,33 +132,38 @@ app.post('/api/user/:userId/favorites', async (req, res) => {
       await user.save();
     }
 
-    res.status(200).json({ message: "Favorite added successfully", favorites: user.favorites });
+    res.status(200).json({
+      message: "Favorite added successfully",
+      favorites: user.favorites,
+    });
   } catch (e) {
     res.status(500).json({ message: "Error adding favorite", error: e });
   }
 });
 
-app.delete('/api/user/:userId/favorites', async (req, res) => {
+app.delete("/api/user/:userId/favorites", async (req, res) => {
   const { userId } = req.params;
   const { itemId } = req.body;
 
   try {
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
-    user.favorites = user.favorites.filter(fav => !fav.equals(itemId));
+    user.favorites = user.favorites.filter((fav) => !fav.equals(itemId));
     await user.save();
 
-    res.status(200).json({ message: "Favorite removed successfully", favorites: user.favorites });
+    res.status(200).json({
+      message: "Favorite removed successfully",
+      favorites: user.favorites,
+    });
   } catch (e) {
     res.status(500).json({ message: "Error removing favorite", error: e });
   }
 });
 
-
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
@@ -172,60 +179,62 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const userDoc = await User.findOne({ email });
     if (!userDoc) {
-      return res.status(404).json('User not found');
+      return res.status(404).json("User not found");
     }
 
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
-      const token = jwt.sign({
-        email: userDoc.email,
-        id: userDoc._id
-      }, jwtSecret);
+      const token = jwt.sign(
+        {
+          email: userDoc.email,
+          id: userDoc._id,
+        },
+        jwtSecret
+      );
 
-      res.cookie('token', token).json(userDoc);
+      res.cookie("token", token).json(userDoc);
     } else {
-      res.status(401).json('Invalid password');
+      res.status(401).json("Invalid password");
     }
   } catch (e) {
     res.status(500).json(e);
   }
 });
 
-app.get('/account', async (req, res) => {
+app.get("/account", async (req, res) => {
   const { token } = req.cookies;
   if (!token) {
-    return res.status(401).json('No token provided');
+    return res.status(401).json("No token provided");
   }
 
   try {
     const userData = jwt.verify(token, jwtSecret);
     const user = await User.findById(userData.id);
     if (!user) {
-      return res.status(404).json('User not found');
+      return res.status(404).json("User not found");
     }
 
     res.json({ name: user.name, email: user.email, _id: user._id });
   } catch (e) {
-    res.status(401).json('Invalid token');
+    res.status(401).json("Invalid token");
   }
 });
 
-app.post('/logout', (req, res) => {
-  res.cookie('token', '').json(true);
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
 });
 
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
